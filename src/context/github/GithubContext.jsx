04ec56fe335e44815a24102c39305d195ed810d2
Simon Ball <simonball245@gmail.com>
2024-06-18 15:00:01 +0200

    Add clear users functionality

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -36,11 +36,14 @@ export const GithubProvider = ({ children }) => {
 
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
+        clearUsers,
         searchUsers,
       }}
     >
