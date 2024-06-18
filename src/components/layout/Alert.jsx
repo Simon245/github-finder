ed34c6dbf402aef63e0ac1d8e2f1dbeeb0aab038
Simon Ball <simonball245@gmail.com>
2024-06-18 15:47:26 +0200

    Create an alert component with context and reducer

---
+++ src/components/layout/Alert.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import AlertContext from '../../context/alert/AlertContext';
+
+function Alert() {
+  const { alert } = useContext(AlertContext);
+  console.log(alert);
+  return (
+    alert !== null && (
+      <div className="flex items-center space-x-2 mb-0 absolute top-4 right-4 bg-red-600 p-4 rounded w-72">
+        {alert.type === 'error' && (
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            className="w-6 h-6 stroke-current mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+            ></path>
+          </svg>
+        )}
+        <p className="flex-1 text-base font-semibold leading-7 text-white">
+          <strong>{alert.msg}</strong>
+        </p>
+      </div>
+    )
+  );
+}
+
+export default Alert;
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,16 +1,18 @@
 import { useContext, useState } from 'react';
 import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/alert/AlertContext';
 
 function UserSearch() {
   const [text, setText] = useState('');
   const { users, clearUsers, searchUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      alert('Please enter something');
+      setAlert('Please enter something in the user search', 'error');
     } else {
       searchUsers(text);
       setText('');
